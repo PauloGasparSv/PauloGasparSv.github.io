90468b90fcef9469f6ea4a10c7413b640309ca29
PauloGasparSv <paulo.vale@madeinweb.com.br>
2018-11-05 20:35:31 -0200

    Added less and gulp to the projectclearclearclearclearclear

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 'use strict';
 
-const autoprefixer = require('gulp-autoprefixer');
-const csso = require('gulp-csso');
-const del = require('del');
-const gulp = require('gulp');
-const htmlmin = require('gulp-htmlmin');
-const runSequence = require('run-sequence');
-const less = require('gulp-less');
-const uglify = require('gulp-uglify-es').default;
-const path = require('path');
+const   gulp = require('gulp'),
+        autoprefixer = require('gulp-autoprefixer'),
+        csso = require('gulp-csso'),
+        del = require('del'),
+        htmlmin = require('gulp-htmlmin'),
+        runSequence = require('run-sequence'),
+        less = require('gulp-less'),
+        uglify = require('gulp-uglify-es').default,
+        path = require('path');
 
+//Added this just because
 const AUTOPREFIXER_BROWSERS = [
     'ie >= 10',
     'ie_mob >= 10',
@@ -22,25 +23,31 @@ const AUTOPREFIXER_BROWSERS = [
     'bb >= 10'
 ];
 
-
+//Cleaning Tasks
 gulp.task('clean', () => del(['public']));
 
+//Task
 gulp.task('less', () =>
     gulp.src('./assets/styles/**/app.less')
     .pipe(less({paths: [ path.join(__dirname, 'less', 'includes') ]}))
     .pipe(gulp.dest('./public/css'))
 );
-
 gulp.task('scripts', () => 
     gulp.src('./assets/js/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./public/js'))
 );
-
 gulp.task('pages', () =>
     gulp.src(['./**/*.html'])
     .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
     .pipe(gulp.dest('./public'))
 );
 
+//Watchers For Dev
+gulp.task('watch', () => {
+    gulp.watch('./assets/styles/**/*.less', gulp.parallel('less'));
+    gulp.watch('./assets/js/**/*.js', gulp.parallel('scripts'));
+});
+
+//The main gulp command for building
 gulp.task('default', gulp.parallel('less','scripts','pages'));
