90b031f6c9a6028fa81b0ff71e5440e17c3d4063
Paulo Vale <paulovale@madeinweb.com.br>
2019-01-11 23:36:05 -0200

    Starting over again. Added browser-sync. Changed the gulp script to use browser-sync and to run as 'npm run serve'.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,12 @@
 'use strict';
 
 const   gulp = require('gulp'),
-        autoprefixer = require('gulp-autoprefixer'),
-        csso = require('gulp-csso'),
         del = require('del'),
         htmlmin = require('gulp-htmlmin'),
-        runSequence = require('run-sequence'),
         less = require('gulp-less'),
         uglify = require('gulp-uglify-es').default,
-        path = require('path');
-
-//Added this just because
-const AUTOPREFIXER_BROWSERS = [
-    'ie >= 10',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 4.0',
-    'bb >= 10'
-];
+        path = require('path'),
+        bs = require('browser-sync').create();
 
 //Cleaning Tasks
 gulp.task('clean', () => del(['public']));
@@ -29,25 +14,37 @@ gulp.task('clean', () => del(['public']));
 //Task
 gulp.task('less', () =>
     gulp.src('./assets/styles/**/app.less')
-    .pipe(less({paths: [ path.join(__dirname, 'less', 'includes') ]}))
+    .pipe(less({
+        paths: [path.join(__dirname, 'less', 'includes')]
+    }))
     .pipe(gulp.dest('./public/css'))
+    .pipe(bs.reload({stream: true}))
 );
 gulp.task('scripts', () => 
     gulp.src('./assets/js/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./public/js'))
+    .pipe(bs.reload({stream: true}))
 );
 gulp.task('pages', () =>
     gulp.src(['./**/*.html'])
-    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+    .pipe(htmlmin({
+        collapseWhitespace: true, removeComments: true
+    }))
     .pipe(gulp.dest('./public'))
 );
 
 //Watchers For Dev
-gulp.task('watch', () => {
+gulp.task('watch-and-serve', () => {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
     gulp.watch('./assets/styles/**/*.less', gulp.parallel('less'));
     gulp.watch('./assets/js/**/*.js', gulp.parallel('scripts'));
-});
+    gulp.watch("./**/*.html").on('change', bs.reload);
+}); 
 
 //The main gulp command for building
 gulp.task('default', gulp.parallel('less','scripts','pages'));
