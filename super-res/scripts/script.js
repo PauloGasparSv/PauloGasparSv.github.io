03f0289e3a871d6bc5a2796473c2db8377b5ac81
Ettore Marques <ettore.marques@madeinweb.com.br>
2020-05-12 20:35:51 -0300

    refining side-menu and burger mobile

--- super-res/scripts/script.js
+++ super-res/scripts/script.js
@@ -1,15 +1,17 @@
 window.addEventListener('DOMContentLoaded', function() {
     const burger = document.querySelector('.burger');
-    const menu = document.querySelector('nav > ul');
+    const menu = document.querySelector('nav .side-nav-content');
     const invisilayer = document.querySelector('.invisilayer');
     function handleMenu() {
         if (burger.classList.contains('active')) {
+            burger.classList.add('closed');
             burger.classList.remove('active');
             menu.classList.remove('active');
             invisilayer.classList.remove('active');
         } else {
             burger.classList.add('active');
             menu.classList.add('active');
+            burger.classList.remove('closed');
             invisilayer.classList.add('active');
         }
     }
